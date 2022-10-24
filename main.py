9e3004d28d64db21b9856f4146229604bcefbf79
Flo <62435410+FRFlo@users.noreply.github.com>
2022-10-24 16:38:47 +0200

    Changement du support de fichiers

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from function import *
 from tkinter import *
+from tkinter import ttk
 from tkinter.filedialog import askopenfilename
 from tkinter.simpledialog import askstring
 from functools import partial
@@ -12,7 +13,7 @@ class App(Tk):
     def __init__(self):
         Tk.__init__(self)
         self.menubar()
-        self.makeentry()
+        self.maketable()
 
         # Propriétés de la fenêtre
 
@@ -25,7 +26,7 @@ class App(Tk):
 
         filemenu = Menu(menu_bar, tearoff=0)
         filemenu.add_command(label="Ouvrir", command=self.choosefile)
-        filemenu.add_command(label="Fermer", command=self.makeentry)
+        filemenu.add_command(label="Fermer", command=self.updatetable)
         filemenu.add_separator()
         filemenu.add_command(label="Quitter", command=self.quit)
         menu_bar.add_cascade(label="Fichier", menu=filemenu)
@@ -41,26 +42,35 @@ class App(Tk):
 
         self.config(menu=menu_bar)
 
-    def makeentry(self, data=None):
-        for widget in self.winfo_children():
-            widget.destroy()
+    def maketable(self):
+        frame = Frame(self)
+        frame.config(background="#121212")
+        frame.pack(fill=BOTH, expand=True)
 
-        self.menubar()
+        scrollbar = Scrollbar(frame, orient=VERTICAL)
+        scrollbar.pack(side=RIGHT, fill=Y)
 
-        if not data:
-            nodata = Label(self, text="Aucunes données",
-                           width=20, fg="white", bg="#3B3B3B")
-            nodata.grid(column=0, row=0, padx=1, pady=1)
-            nodata.place(anchor="center", relx=0.5, rely=0.5)
-        else:
-            for y, elt in enumerate(data):
-                for i, value in enumerate(elt):
-                    item = Label(self, text=value,
-                                 width=20, fg="white", bg="#3B3B3B")
-                    item.grid(column=i, row=y, padx=1, pady=1)
+        self.table = ttk.Treeview(
+            frame, yscrollcommand=scrollbar.set, xscrollcommand=scrollbar.set)
+        self.table.pack(fill=BOTH, expand=True)
 
-    def scrollbar(self):
-        Scrollbar()
+        scrollbar.config(command=self.table.yview)
+        scrollbar.config(command=self.table.xview)
+
+        self.table['columns'] = self.columns
+
+    def updatetable(self, columns=(), data=None):
+        self.table.column("#0", width=0, stretch=NO)
+        self.table.heading("#0", text="", anchor=CENTER)
+        self.table['columns'] = columns
+        for key in columns:
+            self.table.column(key, anchor=CENTER, width=80)
+            self.table.heading(key, text=key, anchor=CENTER)
+
+        for index, key in enumerate(data):
+            self.table.insert(parent='', index='end', iid=index, text='',
+                              values=key)
+        self.table.pack(fill=BOTH, expand=True)
 
     def choosefile(self):
         file = askopenfilename(title="Ouvrir un fichier CSV", filetypes=[(
@@ -68,15 +78,15 @@ class App(Tk):
 
         self.columns, self.cache = cache(file)
         self.icolumns = {k: v for v, k in enumerate(self.columns)}
-        self.makeentry(self.cache)
+        self.updatetable(self.columns, self.cache)
 
     def filter(self, key=None):
         if key:
             value = askstring("Filtrer", "Valeur à filtrer")
-            self.makeentry(listfilter(
+            self.updatetable(listfilter(
                 self.cache, self.icolumns, key, value))
         else:
-            self.makeentry(self.cache)
+            self.updatetable(self.cache)
 
 
 App().mainloop()
